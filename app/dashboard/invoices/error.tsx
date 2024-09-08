4617eeaa43467059052c25b25e444a0471024e57
James <26018087+Jameschan215@user.noreply.github.com>
2024-09-08 12:34:28 +0800

    Chapter 13

---
+++ app/dashboard/invoices/error.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { useEffect } from 'react';
+
+type TError = {
+	error: Error & { digest?: string };
+	reset: () => void;
+};
+
+export default function Error({ error, reset }: TError) {
+	useEffect(() => {
+		console.error(error);
+	}, [error]);
+
+	return (
+		<main className="h-full flex flex-col items-center justify-center">
+			<h2 className="text-center">Something went wrong!</h2>
+			<button className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400">
+				Reset
+			</button>
+		</main>
+	);
+}
